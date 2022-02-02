ed1e7cb9694a64824cc56117a4c3411e33271d86
Jeremy Rybicki <jeremyrybicki@gmail.com>
2022-02-01 20:12:49 -0500

    Actually use the configurable inputs

--- chip8/main.cpp
+++ chip8/main.cpp
@@ -268,46 +268,46 @@ void keyboardDown(unsigned char key, int x, int y)
         exit(0);
     }
 
-    if (key == '1')		    myChip8.key[0x1] = 1;
-    else if (key == '2')	myChip8.key[0x2] = 1;
-    else if (key == '3')	myChip8.key[0x3] = 1;
-    else if (key == '4')	myChip8.key[0xC] = 1;
+    if (key == keyConfig[0])        myChip8.key[0x1] = 1;
+    else if (key == keyConfig[1])	myChip8.key[0x2] = 1;
+    else if (key == keyConfig[2])	myChip8.key[0x3] = 1;
+    else if (key == keyConfig[3])	myChip8.key[0xC] = 1;
 
-    else if (key == 'q')	myChip8.key[0x4] = 1;
-    else if (key == 'w')	myChip8.key[0x5] = 1;
-    else if (key == 'e')	myChip8.key[0x6] = 1;
-    else if (key == 'r')	myChip8.key[0xD] = 1;
+    else if (key == keyConfig[4])	myChip8.key[0x4] = 1;
+    else if (key == keyConfig[5])	myChip8.key[0x5] = 1;
+    else if (key == keyConfig[6])	myChip8.key[0x6] = 1;
+    else if (key == keyConfig[7])	myChip8.key[0xD] = 1;
 
-    else if (key == 'a')	myChip8.key[0x7] = 1;
-    else if (key == 's')	myChip8.key[0x8] = 1;
-    else if (key == 'd')	myChip8.key[0x9] = 1;
-    else if (key == 'f')	myChip8.key[0xE] = 1;
+    else if (key == keyConfig[8])	myChip8.key[0x7] = 1;
+    else if (key == keyConfig[9])	myChip8.key[0x8] = 1;
+    else if (key == keyConfig[10])	myChip8.key[0x9] = 1;
+    else if (key == keyConfig[11])	myChip8.key[0xE] = 1;
 
-    else if (key == 'z')	myChip8.key[0xA] = 1;
-    else if (key == 'x')	myChip8.key[0x0] = 1;
-    else if (key == 'c')	myChip8.key[0xB] = 1;
-    else if (key == 'v')	myChip8.key[0xF] = 1;
+    else if (key == keyConfig[12])	myChip8.key[0xA] = 1;
+    else if (key == keyConfig[13])	myChip8.key[0x0] = 1;
+    else if (key == keyConfig[14])	myChip8.key[0xB] = 1;
+    else if (key == keyConfig[15])	myChip8.key[0xF] = 1;
 }
 
 void keyboardUp(unsigned char key, int x, int y)
 {
-    if (key == '1')		    myChip8.key[0x1] = 0;
-    else if (key == '2')	myChip8.key[0x2] = 0;
-    else if (key == '3')	myChip8.key[0x3] = 0;
-    else if (key == '4')	myChip8.key[0xC] = 0;
+    if (key == keyConfig[0])        myChip8.key[0x1] = 0;
+    else if (key == keyConfig[1])	myChip8.key[0x2] = 0;
+    else if (key == keyConfig[2])	myChip8.key[0x3] = 0;
+    else if (key == keyConfig[3])	myChip8.key[0xC] = 0;
 
-    else if (key == 'q')	myChip8.key[0x4] = 0;
-    else if (key == 'w')	myChip8.key[0x5] = 0;
-    else if (key == 'e')	myChip8.key[0x6] = 0;
-    else if (key == 'r')	myChip8.key[0xD] = 0;
+    else if (key == keyConfig[4])	myChip8.key[0x4] = 0;
+    else if (key == keyConfig[5])	myChip8.key[0x5] = 0;
+    else if (key == keyConfig[6])	myChip8.key[0x6] = 0;
+    else if (key == keyConfig[7])	myChip8.key[0xD] = 0;
 
-    else if (key == 'a')	myChip8.key[0x7] = 0;
-    else if (key == 's')	myChip8.key[0x8] = 0;
-    else if (key == 'd')	myChip8.key[0x9] = 0;
-    else if (key == 'f')	myChip8.key[0xE] = 0;
+    else if (key == keyConfig[8])	myChip8.key[0x7] = 0;
+    else if (key == keyConfig[9])	myChip8.key[0x8] = 0;
+    else if (key == keyConfig[10])	myChip8.key[0x9] = 0;
+    else if (key == keyConfig[11])	myChip8.key[0xE] = 0;
 
-    else if (key == 'z')	myChip8.key[0xA] = 0;
-    else if (key == 'x')	myChip8.key[0x0] = 0;
-    else if (key == 'c')	myChip8.key[0xB] = 0;
-    else if (key == 'v')	myChip8.key[0xF] = 0;
+    else if (key == keyConfig[12])	myChip8.key[0xA] = 0;
+    else if (key == keyConfig[13])	myChip8.key[0x0] = 0;
+    else if (key == keyConfig[14])	myChip8.key[0xB] = 0;
+    else if (key == keyConfig[15])	myChip8.key[0xF] = 0;
 }
